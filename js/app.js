6cf39e36d76605f9a0da23c8d84e3fe26da4d336
Jacek Rajba <jrajba@gmail.com>
2017-03-24 17:26:05 +0100

    poprawiono js oraz naprawiono błędy menu mobilnego i desktopowego

--- js/app.js
+++ js/app.js
@@ -46,36 +46,55 @@ $(function() {
         if ($(this).hasClass("box-button1")) {
             allTextBoxes.hide();
             textBox1.show(300);
-            buttons.removeClass("clicked-box");
+            buttons.removeClass("clicked-tablet2 clicked-tablet");
+            $(this).addClass("clicked-tablet2");
+            $(this).addClass("box-button11");
+            buttons.removeClass("clicked-box box-button22 box-button33 box-button66");
             $(this).addClass("clicked-box");
-
         }else if ($(this).hasClass("box-button2")) {
             allTextBoxes.hide();
             textBox2.toggle(300);
-            buttons.removeClass("clicked-box");
+            buttons.removeClass("clicked-tablet2 clicked-tablet");
+            $(this).addClass("clicked-tablet2");
+            $(this).addClass("box-button22");
+            buttons.removeClass("clicked-box box-button11 box-button33 box-button66");
             $(this).addClass("clicked-box");
         }else if ($(this).hasClass("box-button3")) {
             allTextBoxes.hide();
             textBox3.show(300);
-            buttons.removeClass("clicked-box");
+            buttons.removeClass("clicked-tablet clicked-tablet2");
+            $(this).addClass("clicked-tablet");
+            $(this).addClass("box-button33");
+            buttons.removeClass("clicked-box box-button11 box-button22 box-button66");
             $(this).addClass("clicked-box");
         }else if ($(this).hasClass("phone-button1")) {
             allTextBoxes.hide();
+            buttons.removeClass("clicked-phone");
+            $(this).addClass("clicked-phone");
             textBox1.show(300);
             buttons.removeClass("clicked-box");
             $(this).addClass("clicked-box");
         }else if ($(this).hasClass("box-button6")) {
             allTextBoxes.hide();
+            buttons.removeClass("clicked-phone");
+            $(this).addClass("clicked-phone");
+            buttons.removeClass("clicked-tablet clicked-tablet2");
+            $(this).addClass("clicked-tablet");
             textBox4.show(300);
-            buttons.removeClass("clicked-box");
+            $(this).addClass("box-button66");
+            buttons.removeClass("clicked-box box-button11 box-button33 box-button22");
             $(this).addClass("clicked-box");
         }else if ($(this).hasClass("box-button4")) {
             allTextBoxes.hide();
+            buttons.removeClass("clicked-phone");
+            $(this).addClass("clicked-phone");
             textBox2.show(300);
             buttons.removeClass("clicked-box");
             $(this).addClass("clicked-box");
         }else if ($(this).hasClass("box-button5")) {
             allTextBoxes.hide();
+            buttons.removeClass("clicked-phone");
+            $(this).addClass("clicked-phone");
             textBox3.show(300);
             buttons.removeClass("clicked-box");
             $(this).addClass("clicked-box");
